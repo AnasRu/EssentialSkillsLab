4e5cbab2249786a36cf9a59331f211f3f20d2b9e
TikiTk <mphahleletk@gmail.com>
2017-09-23 13:02:36 +0300

    feature: updating a task

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import Flask, abort
 from flask.helpers import make_response
 from flask.json import jsonify
 from flask import request
+from pkg_resources._vendor.appdirs import unicode
+
 app = Flask(__name__)
 
 tasks = [{'id':1,
@@ -43,6 +45,26 @@ def add_task():
     }
     tasks.append(task)
     return jsonify({'task':task}),201
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>',methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if( 'title' in request.json and type(request.json['title']) != unicode):
+        abort(400)
+    if( 'description' in request.json and type(request.json['description']) != unicode):
+        abort(400)
+    if( 'done' in request.json and type(request.json['done'])!= bool):
+        abort(400)
+
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description',task[0]['description'])
+    task[0]['done'] = request.json.get('done',task[0]['done'])
+
+    return jsonify({'task':task[0]})
 
 
 
